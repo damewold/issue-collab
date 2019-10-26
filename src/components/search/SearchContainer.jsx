e67454888391fdd760ceaba422ca3a8844070963
trybick <t.rybicki@gmail.com>
2019-10-26 18:07:55 -0400

    Style SearchContainer and Loading Spinner

--- src/components/search/SearchContainer.jsx
+++ src/components/search/SearchContainer.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import KeywordsInput from './KeywordsInput';
 import Button from '@material-ui/core/Button';
+import './SearchContainer.scss';
 
 const SearchContainer = ({
   handleTextChange,
@@ -9,28 +10,32 @@ const SearchContainer = ({
   getIssues,
   onReset,
 }) => (
-  <div>
-    <KeywordsInput handleTextChange={handleTextChange} textToSearch={textToSearch} />
-    <Button
-      className="get-btn"
-      color="primary"
-      variant="contained"
-      disabled={isGetButtonDisabled}
-      onClick={getIssues}
-      type="submit"
-      size="large"
-    >
-      Load Issues
-    </Button>
-    <Button
-      color="secondary"
-      variant="contained"
-      className="reset-btn"
-      onClick={onReset}
-      size="large"
-    >
-      Reset
-    </Button>
+  <div className="search-container">
+    <div className="search-container-input">
+      <KeywordsInput handleTextChange={handleTextChange} textToSearch={textToSearch} />
+    </div>
+    <div className="button-group">
+      <Button
+        className="get-btn"
+        color="primary"
+        variant="contained"
+        disabled={isGetButtonDisabled}
+        onClick={getIssues}
+        type="submit"
+        size="large"
+      >
+        Load Issues
+      </Button>
+      <Button
+        color="secondary"
+        variant="contained"
+        className="reset-btn"
+        onClick={onReset}
+        size="large"
+      >
+        Reset
+      </Button>
+    </div>
   </div>
 );
 
