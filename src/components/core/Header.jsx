71a366db762f9157f9250c76a96a28e180ed36ff
trybick <t.rybicki@gmail.com>
2019-10-26 17:49:06 -0400

    Finish styling Header

--- src/components/core/Header.jsx
+++ src/components/core/Header.jsx
@@ -2,11 +2,9 @@ import React from 'react';
 import { GithubCorner } from './GithubCorner';
 import './Header.scss';
 
-// @todo replace inline color style with classes
-
 const Header = () => (
   <div className="page-header">
-    <h2 className="page-title">
+    <h2 className="page-header-title">
       <span style={{ color: '#3498db' }}>Issue</span>-<span style={{ color: 'black' }}>Collab</span>
     </h2>
     {GithubCorner}
