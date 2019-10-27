857d619b80018077265c04169b13e0365a39a3f0
trybick <t.rybicki@gmail.com>
2019-10-27 09:05:10 -0400

    Add a github icon to header

--- src/components/core/Header.jsx
+++ src/components/core/Header.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { GithubCorner } from './GithubCorner';
 import './Header.scss';
+import icon from '../../images/github-icon.png';
 
 const Header = () => (
   <div className="page-header">
     <h2 className="page-header-title">
       <span style={{ color: '#3498db' }}>Issue</span>-<span style={{ color: 'black' }}>Collab</span>
     </h2>
-    {GithubCorner}
+    <img className="github-icon" src={icon} alt="github-logo" />
+    {/* {GithubCorner} */}
   </div>
 );
 
